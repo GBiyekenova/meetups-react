e71d5c4d6228cd7c3aa0de9dece1b5b569a6b365
Gulbike Biyekenova <gbknva@gmail.com>
2022-02-03 10:02:48 -0500

    sends a POST fetch request to firebaseio

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
+import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-practice-app-ca21d-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
+
   return (
-    <div> NewMeetupPage</div>
-  )
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
