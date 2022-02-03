d543ef2d83e6de2ef4ebcbc95d05881642b2b689
Gulbike Biyekenova <gbknva@gmail.com>
2022-02-03 10:05:32 -0500

    add favourites count to the nav bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
-import classes from './MainNavigation.module.css'
+import classes from "./MainNavigation.module.css";
+import FavouritesContext from "../../store/favourites-context";
 
 function MainNavigation() {
+  const favouritesCtx = useContext(FavouritesContext);
   return (
-    <header className={classes.header} >
+    <header className={classes.header}>
       <div className={classes.logo}> React Meetups</div>
       <nav>
         <ul>
@@ -15,7 +18,12 @@ function MainNavigation() {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favourites"> My Favourites</Link>
+            <Link to="/favourites">
+              My Favourites
+              <span className={classes.badge}>
+                {favouritesCtx.totalFavourites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
