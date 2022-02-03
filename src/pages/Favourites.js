1156d65bb0240002256d9cd00e506a48660d8e79
Gulbike Biyekenova <gbknva@gmail.com>
2022-02-03 10:04:25 -0500

    fill out favourites page with content

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,6 +1,23 @@
+import { useContext } from 'react';
+
+import FavouritesContext from '../store/favourites-context';
+import MeetupList from '../components/layout/meetups/MeetupList';
+
 function FavouritesPage() {
+  const favouritesCtx = useContext(FavouritesContext);
+
+  let content;
+
+  if (favouritesCtx.totalFavourites === 0) {
+    content = <p>You got no favourites. Start adding some?</p>
+  } else {
+    content = <MeetupList meetups={favouritesCtx.favourites}/>
+  }
   return (
-    <div> FavouritesPage</div>
+    <section>
+      <h1>My Favouries</h1>
+      {content}
+    </section>
   )
 }
 
