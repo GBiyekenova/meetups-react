d229d2ce1bb3c6d21ea4439f260861f975cb69b1
Gulbike Biyekenova <gbknva@gmail.com>
2022-02-10 14:09:51 -0500

    format the code and add screenshots

--- src/components/layout/meetups/MeetupItem.js
+++ src/components/layout/meetups/MeetupItem.js
@@ -1,8 +1,8 @@
-import { useContext } from 'react'
+import { useContext } from "react";
 
 import classes from "./MeetupItem.module.css";
 import Card from "../../../ui/Card";
-import FavouritesContext from '../../../store/favourites-context';
+import FavouritesContext from "../../../store/favourites-context";
 
 function MeetupItem(props) {
   const favouritesCtx = useContext(FavouritesContext);
@@ -11,33 +11,34 @@ function MeetupItem(props) {
 
   function toggleFavouriteStatusHandler() {
     if (itemIsFavourite) {
-      favouritesCtx.removeFavourite(props.id)
+      favouritesCtx.removeFavourite(props.id);
     } else {
       favouritesCtx.addFavourite({
         id: props.id,
         title: props.title,
         description: props.description,
         image: props.image,
-        address: props.address
-      })
+        address: props.address,
+      });
     }
-
   }
 
   return (
     <li className={classes.item}>
       <Card>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button onClick={toggleFavouriteStatusHandler}>{itemIsFavourite ? 'Remove from Favourites' : 'To Favourites'}</button>
-      </div>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={toggleFavouriteStatusHandler}>
+            {itemIsFavourite ? "Remove from Favourites" : "To Favourites"}
+          </button>
+        </div>
       </Card>
     </li>
   );
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import FavouritesContext from '../store/favourites-context';
-import MeetupList from '../components/layout/meetups/MeetupList';
+import FavouritesContext from "../store/favourites-context";
+import MeetupList from "../components/layout/meetups/MeetupList";
 
 function FavouritesPage() {
   const favouritesCtx = useContext(FavouritesContext);
@@ -9,16 +9,16 @@ function FavouritesPage() {
   let content;
 
   if (favouritesCtx.totalFavourites === 0) {
-    content = <p>You got no favourites. Start adding some?</p>
+    content = <p>You got no favourites. Start adding some?</p>;
   } else {
-    content = <MeetupList meetups={favouritesCtx.favourites}/>
+    content = <MeetupList meetups={favouritesCtx.favourites} />;
   }
   return (
     <section>
       <h1>My Favouries</h1>
       {content}
     </section>
-  )
+  );
 }
 
 export default FavouritesPage;
